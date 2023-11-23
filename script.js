982c27642dbcfa5d8e9cc4c590f006f16be2c9ae
Atom <nykorak19@gmail.com>
2023-11-23 09:45:54 +0200

    les 24

---
+++ script.js
@@ -0,0 +1,76 @@
+
+class Hamburger {
+    constructor(size, stuffing) {
+        this.size = size;
+        this.stuffing = stuffing;
+        this.toppings = [];
+    }
+
+    static SIZE_SMALL = {
+        price: 50,
+        calories: 20
+    };
+
+    static SIZE_LARGE = {
+        price: 100,
+        calories: 40
+    };
+
+    static STUFFING_CHEESE = {
+        price: 10,
+        calories: 20
+    };
+
+    static STUFFING_SALAD = {
+        price: 20,
+        calories: 5
+    };
+
+    static STUFFING_POTATO = {
+        price: 15,
+        calories: 10
+    };
+
+    static TOPPING_SPICE = {
+        price: 15,
+        calories: 0
+    };
+
+    static TOPPING_MAYO = {
+        price: 20,
+        calories: 5
+    };
+
+
+    addTopping(topping) {
+        this.toppings.push(topping);
+    }
+
+    calculateCalories() {
+        const sizeCalories = this.size.calories;
+        const stuffingCalories = this.stuffing.calories;
+        const toppingsCalories = this.toppings.reduce((acc, topping) => acc + topping.calories, 0);
+        return sizeCalories + stuffingCalories + toppingsCalories;
+    }
+
+    calculatePrice() {
+        const sizePrice = this.size.price;
+        const stuffingPrice = this.stuffing.price;
+        const toppingsPrice = this.toppings.reduce((acc, topping) => acc + topping.price, 0);
+        return sizePrice + stuffingPrice + toppingsPrice;
+    }
+}
+
+
+const hamburger = new Hamburger(Hamburger.SIZE_SMALL, Hamburger.STUFFING_CHEESE);
+
+hamburger.addTopping(Hamburger.TOPPING_MAYO);
+
+
+console.log("Calories: " + hamburger.calculateCalories());
+console.log("Price: " + hamburger.calculatePrice());
+
+
+hamburger.addTopping(Hamburger.TOPPING_SPICE);
+
+console.log("Price with spice: " + hamburger.calculatePrice());
